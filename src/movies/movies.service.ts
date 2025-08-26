eb1dd7417618b81188aed7cb79cd6f6e14e2061c
Hamilton Ude Ikedi <hamilton.ude@decagon.dev>
2025-08-26 22:53:49 +0100

    pm2

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -12,7 +12,7 @@ export class MoviesService {
       const response = await axios.get(
         'https://gist.githubusercontent.com/saniyusuf/406b843afdfb9c6a86e25753fe2761f4/raw/523c324c7fcc36efab8224f9ebb7556c09b69a14/Film.JSON',
       );
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       this.movies = response.data;
     } catch (error) {
       console.error('Error fetching movies:', error);
